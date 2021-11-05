3279d68b035e71e91f3daa4c176dd966189b074f
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-05 14:34:26 +0100

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "Engine.hpp"
+
+using namespace spic;
+
+int main(int argc, char* args[]) {
+//    Engine& engine = Engine::Instance();
+//
+//    EngineConfig config{{"Avans Wars", 1366, 786, true}};
+//    engine.Init(config);
+//
+//    auto scene = std::make_shared<Scene>();
+//    engine.PushScene(scene);
+//
+//    engine.Start();
+}
