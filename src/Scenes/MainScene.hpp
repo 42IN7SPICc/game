ddcc36990249ca1522141c8b9600ed3807be2752
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-15 15:46:04 +0100

    Create MainScene

---
+++ src/Scenes/MainScene.hpp
@@ -0,0 +1,12 @@
+#ifndef SPIC_GAME_MAINSCENE_HPP
+#define SPIC_GAME_MAINSCENE_HPP
+
+#include "Scene.hpp"
+
+namespace game {
+    class MainScene : spic::Scene {
+
+    };
+}
+
+#endif //SPIC_GAME_MAINSCENE_HPP
