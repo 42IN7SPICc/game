30decbe5306376059fa38032c80a79cde62563fb
Tommy Hosewol <t.hosewol@gmail.com>
2021-11-15 16:14:56 +0100

    Created string helper
    Created string helper with 2 methods : StartsWith and EndsWith

---
+++ src/Utils/StringHelper.hpp
@@ -0,0 +1,14 @@
+#ifndef SPIC_GAME_STRINGHELPER_H
+#define SPIC_GAME_STRINGHELPER_H
+
+#include <string>
+
+namespace game {
+    class StringHelper {
+    public:
+        static bool EndsWith(const std::string& haystack, const std::string& needle);
+        static bool StartsWith(const std::string& haystack, const std::string& needle);
+    };
+}
+
+#endif
