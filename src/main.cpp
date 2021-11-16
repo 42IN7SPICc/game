27bb1eb478e246caa6fd315c024d54a00f2cc08b
daanh432 <8281824+daanh432@users.noreply.github.com>
2021-11-16 12:45:30 +0100

    Reset main

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,15 @@
-#include "Engine.hpp"
-#include "GameObject.hpp"
-#include "scripts/common/UserMovementBehaviour.hpp"
-#include <memory>
-#include <Sprite.hpp>
+#include "Api.hpp"
 
 using namespace spic;
 
 int main(int argc, char* args[]) {
     Engine& engine = Engine::Instance();
 
-    EngineConfig config{{"Avans Wars", 1366, 786, false}};
+    EngineConfig config{{"Avans Wars", 1366, 786, true}};
     engine.Init(config);
 
     auto scene = std::make_shared<Scene>();
     engine.PushScene(scene);
-
-    auto player = std::make_shared<GameObject>("character", "player", 0);
-    scene->Contents().push_back(player);
-
-    auto sprite = std::make_shared<spic::Sprite>("", false, false, 0, 0);
-    sprite->GameObject(player);
-    player->AddComponent(sprite);
-
-    auto movementBehaviour = std::make_shared<game::UserMovementBehaviour>(1.5);
-    movementBehaviour->GameObject(player);
-    player->AddComponent(movementBehaviour);
 
     engine.Start();
 }
