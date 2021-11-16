1456214deeacbacf09b0ac0cc7f7a0e5b1961f7e
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-11-16 12:31:20 +0100

    Removed last edited code

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,11 @@ using namespace spic;
 int main(int argc, char* args[]) {
     Engine& engine = Engine::Instance();
 
-    EngineConfig config{{"Avans Wars", 1366, 786, false}};
+    EngineConfig config{{"Avans Wars", 1366, 786, true}};
     engine.Init(config);
 
     auto scene = std::make_shared<Scene>();
     engine.PushScene(scene);
 
-    scene->Contents().push_back(cheats);
-
     engine.Start();
-    return 0;
 }
