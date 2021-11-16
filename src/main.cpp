82568072754e1ac9786a8af88320e243bebda6a4
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-16 13:13:13 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include "Engine.hpp"
 
+#include "Scenes/MainScene.hpp"
+
+using namespace game;
 using namespace spic;
 
-int main(int argc, char* args[]) {
+int main(int argc, char* args[])
+{
     Engine& engine = Engine::Instance();
 
     EngineConfig config{{"Avans Wars", 1366, 786, true}};
+#if !NDEBUG
+    config.window.fullscreen = false;
+#endif
     engine.Init(config);
 
-    auto scene = std::make_shared<Scene>();
+    auto scene = std::make_shared<MainScene>();
     engine.PushScene(scene);
 
     engine.Start();
