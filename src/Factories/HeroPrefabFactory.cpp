c5b702b4260504326c565dcd34c29cfe59b4054d
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-11-16 12:36:49 +0100

    Formatted Current Code

--- src/Factories/HeroPrefabFactory.cpp
+++ src/Factories/HeroPrefabFactory.cpp
@@ -1,8 +1,10 @@
 #include "HeroPrefabFactory.hpp"
 
-std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateHero(game::HeroName name) {
+std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateHero(game::HeroName name)
+{
     auto base_hero = CreateBaseHero();
-    switch (name) {
+    switch (name)
+    {
         case DesmondDoss:
             return CreateDesmondDoss(base_hero);
         case BernardIJzerdraat:
@@ -16,27 +18,33 @@ std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateHero(game::Hero
     }
 }
 
-std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateDesmondDoss(std::shared_ptr<spic::GameObject> base_hero) {
+std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateDesmondDoss(std::shared_ptr<spic::GameObject> base_hero)
+{
     return base_hero;
 }
 
-std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateBernardIJzerdraat(std::shared_ptr<spic::GameObject> base_hero) {
+std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateBernardIJzerdraat(std::shared_ptr<spic::GameObject> base_hero)
+{
     return base_hero;
 }
 
-std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateFranklinDRoosevelt(std::shared_ptr<spic::GameObject> base_hero) {
+std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateFranklinDRoosevelt(std::shared_ptr<spic::GameObject> base_hero)
+{
     return base_hero;
 }
 
-std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateWinstonChurchill(std::shared_ptr<spic::GameObject> base_hero) {
+std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateWinstonChurchill(std::shared_ptr<spic::GameObject> base_hero)
+{
     return base_hero;
 }
 
-std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateJosephStalin(std::shared_ptr<spic::GameObject> base_hero) {
+std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateJosephStalin(std::shared_ptr<spic::GameObject> base_hero)
+{
     return base_hero;
 }
 
-std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateBaseHero() {
+std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateBaseHero()
+{
     auto base_hero = std::shared_ptr<spic::GameObject>();
     return base_hero;
 }
--- src/Factories/HeroPrefabFactory.hpp
+++ src/Factories/HeroPrefabFactory.hpp
@@ -3,8 +3,10 @@
 
 #include "GameObject.hpp"
 
-namespace game {
-    enum HeroName {
+namespace game
+{
+    enum HeroName
+    {
         DesmondDoss,
         BernardIJzerdraat,
         FranklinDRoosevelt,
@@ -12,16 +14,23 @@ namespace game {
         JosephStalin
     };
 
-    class HeroPrefabFactory {
-    private:
-        static std::shared_ptr<spic::GameObject> CreateDesmondDoss(std::shared_ptr<spic::GameObject> base_hero);
-        static std::shared_ptr<spic::GameObject> CreateBernardIJzerdraat(std::shared_ptr<spic::GameObject> base_hero);
-        static std::shared_ptr<spic::GameObject> CreateFranklinDRoosevelt(std::shared_ptr<spic::GameObject> base_hero);
-        static std::shared_ptr<spic::GameObject> CreateWinstonChurchill(std::shared_ptr<spic::GameObject> base_hero);
-        static std::shared_ptr<spic::GameObject> CreateJosephStalin(std::shared_ptr<spic::GameObject> base_hero);
-        static std::shared_ptr<spic::GameObject> CreateBaseHero();
-    public:
-        static std::shared_ptr<spic::GameObject> CreateHero(HeroName name);
+    class HeroPrefabFactory
+    {
+        private:
+            static std::shared_ptr<spic::GameObject> CreateDesmondDoss(std::shared_ptr<spic::GameObject> base_hero);
+
+            static std::shared_ptr<spic::GameObject> CreateBernardIJzerdraat(std::shared_ptr<spic::GameObject> base_hero);
+
+            static std::shared_ptr<spic::GameObject> CreateFranklinDRoosevelt(std::shared_ptr<spic::GameObject> base_hero);
+
+            static std::shared_ptr<spic::GameObject> CreateWinstonChurchill(std::shared_ptr<spic::GameObject> base_hero);
+
+            static std::shared_ptr<spic::GameObject> CreateJosephStalin(std::shared_ptr<spic::GameObject> base_hero);
+
+            static std::shared_ptr<spic::GameObject> CreateBaseHero();
+
+        public:
+            static std::shared_ptr<spic::GameObject> CreateHero(HeroName name);
     };
 }
 
