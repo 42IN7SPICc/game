dbe144ad937dee2cecc1b575d5f4c409d7738de8
daanh432 <daan@daanhendriks.nl>
2021-11-16 12:38:28 +0100

    Updated user movement behaviour header

--- src/Scripts/Common/UserMovementBehaviour.hpp
+++ src/Scripts/Common/UserMovementBehaviour.hpp
@@ -1,24 +1,33 @@
 #ifndef SPIC_GAME_USERMOVEMENTBEHAVIOUR_HPP
 #define SPIC_GAME_USERMOVEMENTBEHAVIOUR_HPP
 
+#include <memory>
 #include <BehaviourScript.hpp>
+#include <RigidBody.hpp>
+#include <Animator.hpp>
 
-namespace game {
-    class UserMovementBehaviour : public spic::BehaviourScript {
-    private:
-        float _velocity;
-    public:
-        explicit UserMovementBehaviour(float velocity);
+namespace game
+{
+    class UserMovementBehaviour : public spic::BehaviourScript
+    {
+        private:
+            float _velocity;
+            std::shared_ptr<spic::GameObject> _parent;
+            std::shared_ptr<spic::RigidBody> _rigidBody;
+            std::shared_ptr<spic::Animator> _idleAnimator;
+            std::shared_ptr<spic::Animator> _walkingAnimator;
+        public:
+            explicit UserMovementBehaviour(float velocity, std::shared_ptr<spic::Animator> idleAnimator, std::shared_ptr<spic::Animator> walkingAnimator);
 
-        void OnStart() override;
+            void OnStart() override;
 
-        void OnUpdate() override;
+            void OnUpdate() override;
 
-        void OnTriggerEnter2D(const spic::Collider &collider) override;
+            void OnTriggerEnter2D(const spic::Collider& collider) override;
 
-        void OnTriggerExit2D(const spic::Collider &collider) override;
+            void OnTriggerExit2D(const spic::Collider& collider) override;
 
-        void OnTriggerStay2D(const spic::Collider &collider) override;
+            void OnTriggerStay2D(const spic::Collider& collider) override;
     };
 }
 
