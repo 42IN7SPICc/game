301141829ec329857ad74046a32d30be4505ba71
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-11-16 13:22:09 +0100

    Added BaseHero Prefab

--- src/Factories/HeroPrefabFactory.cpp
+++ src/Factories/HeroPrefabFactory.cpp
@@ -1,4 +1,12 @@
+#include <Animator.hpp>
 #include "HeroPrefabFactory.hpp"
+#include "Sprite.hpp"
+#include "../Scripts/common/HealthBehaviour.hpp"
+#include "../Scripts/common/UserMovementBehaviour.hpp"
+#include "../Scripts/common/AttackBehaviour.hpp"
+#include "../Utils/GameObjectUtil.hpp"
+
+//TODO: maxHealth, velocity, attackPower
 
 std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateHero(game::HeroName name)
 {
@@ -45,6 +53,50 @@ std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateJosephStalin(st
 
 std::shared_ptr<spic::GameObject> game::HeroPrefabFactory::CreateBaseHero()
 {
-    auto base_hero = std::shared_ptr<spic::GameObject>();
-    return base_hero;
+    auto baseHero = std::make_shared<spic::GameObject>("Hero", "Player", 1);
+
+    auto defaultSprite = std::make_shared<spic::Sprite>("resources/sprites/heroes/Idle/hero_idle_1.png", false, false, 0, 0);
+    GameObjectUtil::LinkComponent(baseHero, defaultSprite);
+
+    std::vector<std::shared_ptr<spic::Sprite>> idleSprites;
+    std::vector<std::shared_ptr<spic::Sprite>> walkingSprites;
+
+    idleSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Idle/hero_idle_1.png", false, false, 0, 0));
+    idleSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Idle/hero_idle_2.png", false, false, 0, 0));
+    idleSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Idle/hero_idle_3.png", false, false, 0, 0));
+    idleSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Idle/hero_idle_4.png", false, false, 0, 0));
+    idleSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Idle/hero_idle_5.png", false, false, 0, 0));
+    idleSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Idle/hero_idle_6.png", false, false, 0, 0));
+    idleSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Idle/hero_idle_7.png", false, false, 0, 0));
+    idleSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Idle/hero_idle_8.png", false, false, 0, 0));
+    idleSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Idle/hero_idle_9.png", false, false, 0, 0));
+    idleSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Idle/hero_idle_10.png", false, false, 0, 0));
+
+    walkingSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Walking/hero_walking_1.png", false, false, 0, 0));
+    walkingSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Walking/hero_walking_2.png", false, false, 0, 0));
+    walkingSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Walking/hero_walking_3.png", false, false, 0, 0));
+    walkingSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Walking/hero_walking_4.png", false, false, 0, 0));
+    walkingSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Walking/hero_walking_5.png", false, false, 0, 0));
+    walkingSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Walking/hero_walking_6.png", false, false, 0, 0));
+    walkingSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Walking/hero_walking_7.png", false, false, 0, 0));
+    walkingSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Walking/hero_walking_8.png", false, false, 0, 0));
+    walkingSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Walking/hero_walking_9.png", false, false, 0, 0));
+    walkingSprites.push_back(std::make_shared<spic::Sprite>("resources/sprites/heroes/Walking/hero_walking_10.png", false, false, 0, 0));
+
+    auto idleAnimator = std::make_shared<spic::Animator>(12, idleSprites);
+    GameObjectUtil::LinkComponent(baseHero, idleAnimator);
+
+    auto walkingAnimator = std::make_shared<spic::Animator>(12, walkingSprites);
+    GameObjectUtil::LinkComponent(baseHero, walkingAnimator);
+
+    auto healthBehaviour = std::make_shared<game::HealthBehaviour>(100);
+    GameObjectUtil::LinkComponent(baseHero, healthBehaviour);
+
+    auto userMovementBehaviour = std::make_shared<game::UserMovementBehaviour>(12, idleAnimator, walkingAnimator);
+    GameObjectUtil::LinkComponent(baseHero, userMovementBehaviour);
+
+    auto attackBehaviour = std::make_shared<game::AttackBehaviour>(12);
+    GameObjectUtil::LinkComponent(baseHero, attackBehaviour);
+
+    return baseHero;
 }
