3a84461120c595f861467264dee6a2656fc085d6
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-16 12:55:22 +0100

    Create Layer Util

---
+++ src/Utils/Layer.hpp
@@ -0,0 +1,14 @@
+#ifndef SPIC_GAME_LAYERS_HPP
+#define SPIC_GAME_LAYERS_HPP
+
+namespace game
+{
+    enum Layer
+    {
+        Background = 1,
+        Game = 10,
+        HUD = 50,
+    };
+}
+
+#endif //SPIC_GAME_LAYERS_HPP
