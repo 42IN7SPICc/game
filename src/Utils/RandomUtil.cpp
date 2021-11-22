c00f6a463364273659bb070e920399bd443e6da3
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-22 15:10:15 +0100

    Update RandomUtil.cpp

--- src/Utils/RandomUtil.cpp
+++ src/Utils/RandomUtil.cpp
@@ -10,7 +10,7 @@ RandomUtil::RandomUtil()
   _engine = std::default_random_engine{device()};
 }
 
-RandomUtil::Next(int min, int max)
+int RandomUtil::Next(int min, int max)
 {
   std::uniform_int_distribution<int> dist{min, max};
   return dist(_instance._engine);
