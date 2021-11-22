cc24405344d96e93ffa1e045e2639fef2c502cab
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-22 16:17:26 +0100

    Update MainScene.cpp

--- src/Scenes/MainScene.cpp
+++ src/Scenes/MainScene.cpp
@@ -1,7 +1,9 @@
 #include "MainScene.hpp"
 #include "LevelScene.hpp"
+#include "CreditScene.hpp"
 
 #include "Api.hpp"
+#include "../Factories/ButtonPrefabFactory.hpp"
 #include "../Factories/HeroPrefabFactory.hpp"
 #include "../Utils/Layer.hpp"
 #include "../Utils/GameObjectUtil.hpp"
@@ -20,8 +22,7 @@ MainScene::MainScene()
     auto titleText = std::make_shared<spic::Text>("Title Text", "text_title", Layer::HUD, 1720, 100, "Avans Wars: WW2", "resources/fonts/capture_it.otf", 82, Alignment::left, Color::white());
     titleText->Transform().position = {100, 100};
 
-    auto playButton = std::make_shared<spic::Button>("Play Button", "button_play", Layer::HUD, 250, 85);
-    playButton->Transform().position = {100, 250};
+    auto playButton = ButtonPrefabFactory::CreateButton("Play Button", "button_play", "PLAY", {100, 250});
     playButton->OnClick([]() {
         LevelController levelController{};
         levelController.InitializeLevels();
@@ -29,14 +30,15 @@ MainScene::MainScene()
         auto scene = std::make_shared<LevelScene>("welcome_to_the_war", levelController);
         Engine::Instance().PushScene(scene);
     });
-    GameObjectUtil::LinkComponent(playButton, std::make_shared<spic::Sprite>("resources/sprites/hud/buttons/play.png", false, false, 0, 0));
 
-    auto exitButton = std::make_shared<spic::Button>("Quit Button", "button_quit", Layer::HUD, 250, 85);
-    exitButton->Transform().position = {100, 400};
+    auto exitButton = ButtonPrefabFactory::CreateButton("Exit Button", "button_exit", "EXIT", {100, 375});
     exitButton->OnClick([]() {
         Engine::Instance().PopScene();
     });
-    GameObjectUtil::LinkComponent(exitButton, std::make_shared<spic::Sprite>("resources/sprites/hud/buttons/exit.png", false, false, 0, 0));
+
+    auto creditsButton = ButtonPrefabFactory::CreateButton("Credits Button", "button_credits", "CREDITS", {100, 500});
+    creditsButton->OnClick([]() {
+    });
 
     auto hero = HeroPrefabFactory::CreateHero(static_cast<HeroName>(RandomUtil::Next(HeroName::DesmondDoss, HeroName::JosephStalin)));
     hero->Transform().position = {900, 300};
@@ -48,4 +50,5 @@ MainScene::MainScene()
     Contents().push_back(titleText);
     Contents().push_back(playButton);
     Contents().push_back(exitButton);
+    Contents().push_back(creditsButton);
 }
