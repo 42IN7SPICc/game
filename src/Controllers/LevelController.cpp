c44c29df4caa431fedeb6ca816c65c83e2644d9b
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-22 13:39:07 +0100

    Comment out rapidjson

--- src/Controllers/LevelController.cpp
+++ src/Controllers/LevelController.cpp
@@ -19,30 +19,30 @@ const int TileSize = 32;
 
 std::shared_ptr<spic::GameObject> LevelController::GetLevelGameObject(const std::string& fileName)
 {
-    auto document = LoadFile(fileName);
-
-    auto tileMap = std::make_shared<spic::GameObject>("TileGrid", "tilemap", Layer::Game);
-
-    if (!document.HasMember("tiles"))
-        throw std::runtime_error(std::string{"The tiles for `" + _levels[fileName].File + "` could not be parsed"});
-
-    const rapidjson::Value& tiles = document["tiles"];
-
-    for (int i = 0; i < tiles.Size(); ++i)
-    {
-        auto name = "Tile_" + std::to_string(i) + "_Type_" + std::to_string(tiles[i]["type"].GetInt());
-
-        auto tile = std::make_shared<spic::GameObject>(name, "tile", Layer::Game);
-        auto sprite = std::make_shared<spic::Sprite>(TileUtil::GetSprite((TileType) tiles[i]["type"].GetInt()), false, false, 1, 1);
-
-        tile->Transform().position.x = tiles[i]["x"].GetInt() * TileSize;
-        tile->Transform().position.y = tiles[i]["y"].GetInt() * TileSize;
-
-        GameObjectUtil::LinkComponent(tile, sprite);
-        GameObjectUtil::LinkChild(tileMap, tile);
-    }
-
-    return tileMap;
+//    auto document = LoadFile(fileName);
+//
+//    auto tileMap = std::make_shared<spic::GameObject>("TileGrid", "tilemap", Layer::Game);
+//
+//    if (!document.HasMember("tiles"))
+//        throw std::runtime_error(std::string{"The tiles for `" + _levels[fileName].File + "` could not be parsed"});
+//
+//    const rapidjson::Value& tiles = document["tiles"];
+//
+//    for (int i = 0; i < tiles.Size(); ++i)
+//    {
+//        auto name = "Tile_" + std::to_string(i) + "_Type_" + std::to_string(tiles[i]["type"].GetInt());
+//
+//        auto tile = std::make_shared<spic::GameObject>(name, "tile", Layer::Game);
+//        auto sprite = std::make_shared<spic::Sprite>(TileUtil::GetSprite((TileType) tiles[i]["type"].GetInt()), false, false, 1, 1);
+//
+//        tile->Transform().position.x = tiles[i]["x"].GetInt() * TileSize;
+//        tile->Transform().position.y = tiles[i]["y"].GetInt() * TileSize;
+//
+//        GameObjectUtil::LinkComponent(tile, sprite);
+//        GameObjectUtil::LinkChild(tileMap, tile);
+//    }
+//
+//    return tileMap;
 }
 
 void LevelController::InitializeLevels()
@@ -66,54 +66,54 @@ void LevelController::InitializeLevels()
 
 void LevelController::InitializeLevel(const std::string& file, const std::string& name)
 {
-    try
-    {
-        auto document = LoadFile(file);
-
-        if (!document.HasMember("title"))
-            throw std::runtime_error(std::string{"The title for `" + file + "` could not be parsed"});
-
-        if (!document.HasMember("description"))
-            throw std::runtime_error(std::string{"The description for `" + file + "` could not be parsed"});
-
-        if (!document.HasMember("unlockThreshold"))
-            throw std::runtime_error(std::string{"The unlockThreshold for `" + file + "` could not be parsed"});
-
-        _levels.insert(std::make_pair(name, Level{
-                document["title"].GetString(),
-                document["description"].GetString(),
-                file,
-                document["unlockThreshold"].GetInt()
-        }));
-    }
-    catch (...)
-    {
-        throw std::runtime_error(std::string{"Failed to initialize level `" + file + "`."});
-    }
+//    try
+//    {
+//        auto document = LoadFile(file);
+//
+//        if (!document.HasMember("title"))
+//            throw std::runtime_error(std::string{"The title for `" + file + "` could not be parsed"});
+//
+//        if (!document.HasMember("description"))
+//            throw std::runtime_error(std::string{"The description for `" + file + "` could not be parsed"});
+//
+//        if (!document.HasMember("unlockThreshold"))
+//            throw std::runtime_error(std::string{"The unlockThreshold for `" + file + "` could not be parsed"});
+//
+//        _levels.insert(std::make_pair(name, Level{
+//                document["title"].GetString(),
+//                document["description"].GetString(),
+//                file,
+//                document["unlockThreshold"].GetInt()
+//        }));
+//    }
+//    catch (...)
+//    {
+//        throw std::runtime_error(std::string{"Failed to initialize level `" + file + "`."});
+//    }
 }
 
-rapidjson::Document LevelController::LoadFile(const std::string& fileName)
-{
-    try
-    {
-        std::ifstream levelFile(fileName);
-        std::string contents((std::istreambuf_iterator<char>(levelFile)), std::istreambuf_iterator<char>());
-
-        levelFile.close();
-        rapidjson::Document document;
-
-        if (document.Parse(contents.c_str()).GetParseError() != rapidjson::kParseErrorNone)
-        {
-            throw std::runtime_error(std::string{"Failed to parse `" + fileName + "`."});
-        }
-
-        return document;
-    }
-    catch (std::exception&)
-    {
-        throw std::runtime_error(std::string{"Something went wrong handling `" + fileName + "`."});
-    }
-}
+//rapidjson::Document LevelController::LoadFile(const std::string& fileName)
+//{
+//    try
+//    {
+//        std::ifstream levelFile(fileName);
+//        std::string contents((std::istreambuf_iterator<char>(levelFile)), std::istreambuf_iterator<char>());
+//
+//        levelFile.close();
+//        rapidjson::Document document;
+//
+//        if (document.Parse(contents.c_str()).GetParseError() != rapidjson::kParseErrorNone)
+//        {
+//            throw std::runtime_error(std::string{"Failed to parse `" + fileName + "`."});
+//        }
+//
+//        return document;
+//    }
+//    catch (std::exception&)
+//    {
+//        throw std::runtime_error(std::string{"Something went wrong handling `" + fileName + "`."});
+//    }
+//}
 
 Level LevelController::GetLevelDto(const std::string& levelName)
 {
