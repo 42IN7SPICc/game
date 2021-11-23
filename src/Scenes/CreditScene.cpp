58cd6096eacb56122f142e29ae1a5ec844b4a952
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-23 12:29:01 +0100

    Update CreditScene.cpp

--- src/Scenes/CreditScene.cpp
+++ src/Scenes/CreditScene.cpp
@@ -13,8 +13,8 @@ using namespace spic;
 
 CreditScene::CreditScene() : MenuScene("Credits", true)
 {
-    auto creditsText = std::make_shared<Text>("Credits Text", "text_credits", Layer::HUD, 1720, 400);
-    creditsText->Transform().position = {100, 250};
+    auto creditsText = std::make_shared<Text>("Credits Text", "text_credits", Layer::HUD, 1166, 400);
+    creditsText->Transform().position = {683, 450};
     creditsText->Size(24);
     creditsText->TextColor(Color::white());
     creditsText->Content("Avans Wars: WW2 has been created by\n\nDaan Hendriks\nMax van de Vossenberg\nSander Jochems\nTommy Hosewol");
