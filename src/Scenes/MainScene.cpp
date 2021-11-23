7882580ef4a3f8bbefdc25113fa7fbd11fb779db
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-23 11:41:58 +0100

    Add MenuScene to MainScene

--- src/Scenes/MainScene.cpp
+++ src/Scenes/MainScene.cpp
@@ -4,11 +4,8 @@
 
 #include "Api.hpp"
 
-#include "../Factories/BackgroundPrefabFactory.hpp"
 #include "../Factories/ButtonPrefabFactory.hpp"
 #include "../Factories/HeroPrefabFactory.hpp"
-#include "../Utils/Layer.hpp"
-#include "../Utils/GameObjectUtil.hpp"
 #include "../Utils/RandomUtil.hpp"
 #include "../Scripts/Common/UserAttackBehaviour.hpp"
 #include "../Scripts/Common/UserMovementBehaviour.hpp"
@@ -16,11 +13,8 @@
 using namespace spic;
 using namespace game;
 
-MainScene::MainScene()
+MainScene::MainScene() : MenuScene("Avans Wars: WW2", false)
 {
-    auto titleText = std::make_shared<spic::Text>("Title Text", "text_title", Layer::HUD, 1720, 100, "Avans Wars: WW2", "resources/fonts/capture_it.otf", 82, Alignment::left, Color::white());
-    titleText->Transform().position = {100, 100};
-
     auto playButton = ButtonPrefabFactory::CreateOutlineButton("Play Button", "button_play", "PLAY");
     playButton->Transform().position = {100, 250};
     playButton->OnClick([]() {
@@ -48,10 +42,7 @@ MainScene::MainScene()
     hero->GetComponent<UserMovementBehaviour>()->Controllable(false);
     hero->RemoveComponent(hero->GetComponent<UserAttackBehaviour>());
 
-    Contents().push_back(BackgroundPrefabFactory::CreateBackground(BackgroundName::Menu));
-
     Contents().push_back(hero);
-    Contents().push_back(titleText);
     Contents().push_back(playButton);
     Contents().push_back(exitButton);
     Contents().push_back(creditsButton);
