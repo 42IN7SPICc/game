a9c32270d4a7282090ae643100f4835339c42758
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-23 16:57:41 +0100

    Create Font

---
+++ src/Utils/Font.cpp
@@ -0,0 +1,9 @@
+#include "Font.hpp"
+
+using namespace game;
+
+const std::string& Font::Default = Arial;
+const std::string& Font::Title = CaptureIt;
+
+const std::string& Font::Arial = "resources/fonts/arial.ttf";
+const std::string& Font::CaptureIt = "resources/fonts/capture_it.ttf";
---
+++ src/Utils/Font.hpp
@@ -0,0 +1,19 @@
+#ifndef SPIC_GAME_FONT_HPP
+#define SPIC_GAME_FONT_HPP
+
+#include <string>
+
+namespace game
+{
+    class Font
+    {
+        public:
+            static const std::string& Default;
+            static const std::string& Title;
+
+            static const std::string& Arial;
+            static const std::string& CaptureIt;
+    };
+}
+
+#endif //SPIC_GAME_FONT_HPP
