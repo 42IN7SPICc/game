1c3cb1e217ad49710bd935d286c5c52de59c7114
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-23 17:07:33 +0100

    Update MenuScene.cpp

--- src/Scenes/MenuScene.cpp
+++ src/Scenes/MenuScene.cpp
@@ -3,6 +3,7 @@
 #include "../Factories/BackgroundPrefabFactory.hpp"
 #include "../Factories/ButtonPrefabFactory.hpp"
 #include "../Utils/Layer.hpp"
+#include "../Utils/Font.hpp"
 
 #include "Text.hpp"
 
@@ -12,21 +13,17 @@
 using namespace game;
 using namespace spic;
 
-const std::string TitleFont = "capture_it.otf";
-
 MenuScene::MenuScene(const std::string& title, bool hasBackButton, BackgroundName backgroundName)
 {
-    if (title.empty())
-    {
-        throw std::runtime_error("The title of a menu cannot be empty.");
-    }
-
     Contents().push_back(BackgroundPrefabFactory::CreateBackground(backgroundName));
 
-    auto titleText = std::make_shared<Text>("Title Text", "text_title", Layer::HUD, 1166, 100, title, "resources/fonts/" + TitleFont, 82, Alignment::left, Color::white());
-    titleText->Transform().position = {683, 141};
+    if (!title.empty())
+    {
+        auto titleText = std::make_shared<Text>("Title Text", "text_title", Layer::HUD, 1166, 100, title, Font::Title, 82, Alignment::left, Color::white());
+        titleText->Transform().position = {683, 141};
 
-    Contents().push_back(titleText);
+        Contents().push_back(titleText);
+    }
 
     if (hasBackButton)
     {
