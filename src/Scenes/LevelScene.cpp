5143dbde7d9528b5261608bf8f7b2fd25f11b9b6
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-11-23 17:51:29 +0100

    Added Buttons and HUD to LevelScene

--- src/Scenes/LevelScene.cpp
+++ src/Scenes/LevelScene.cpp
@@ -1,8 +1,10 @@
 #include "LevelScene.hpp"
 
+#include <algorithm>
 #include "Api.hpp"
 #include "../Utils/Layer.hpp"
 #include "../Utils/GameObjectUtil.hpp"
+#include "../Factories/BackgroundPrefabFactory.hpp"
 
 using namespace spic;
 using namespace game;
@@ -11,20 +13,43 @@ LevelScene::LevelScene(const std::string& levelName, LevelController& levelContr
 {
     auto level = levelController.GetLevelDto(levelName);
 
-    auto background = std::make_shared<spic::GameObject>("Background", "background", Layer::Background);
-    GameObjectUtil::LinkComponent(background, std::make_shared<spic::Sprite>("resources/sprites/menu/backgrounds/mud.png", false, false, 0, 0));
-
+    auto background = BackgroundPrefabFactory::CreateBackground(BackgroundName::Menu);
     auto titleText = std::make_shared<spic::Text>("Title Text", "text_title", Layer::HUD, 1720, 100, level.Title, "resources/fonts/capture_it.otf", 35, Alignment::left, Color::white());
     titleText->Transform().position = {25, 25};
 
     auto tilesMapObject = levelController.GetLevelGameObject(level.File);
-    tilesMapObject->Transform().position.x = 75;
-    tilesMapObject->Transform().position.y = 75;
+    //tilesMapObject->Transform().position.x = 75;
+    //->Transform().position.y = 75;
 
     Contents().push_back(background);
     Contents().push_back(titleText);
-    Contents().push_back(tilesMapObject);
-}{
+    //Contents().push_back(tilesMapObject);
+
+    int width = 1366;
+    int height = 786;
+    int hudWidth = 250;
+
+    auto rightHud = std::make_shared<GameObject>("RightHud", "hud", Layer::HUD);
+    auto rightHudSprite = std::make_shared<spic::Sprite>("resources/sprites/hud/white_block.png", false, false, 100, 1);
+    GameObjectUtil::LinkComponent(rightHud, rightHudSprite);
+
+    rightHud->Transform().position.x = width - (hudWidth / 2.0);
+    rightHud->Transform().position.y = height / 2.0;
+
+    auto streetButton = InitializeTileButton(rightHud, "street.png");
+    streetButton->Transform().position.y = -34 * 4;
+
+    auto grassButton = InitializeTileButton(rightHud, "grass.png");
+    grassButton->Transform().position.y = 0;
+
+    auto sandButton = InitializeTileButton(rightHud, "sand.png");
+    sandButton->Transform().position.y = 34 * 4;
+
+    Contents().push_back(rightHud);
+}
+
+std::shared_ptr<spic::Button> LevelScene::InitializeTileButton(const std::shared_ptr<GameObject>& HUD, const std::string& texture)
+{
     auto button = std::make_shared<spic::Button>("button-tile-" + texture, "tile_button", Layer::HUD, 32,32);
     button->Transform().scale = 4.0;
     auto buttonSprite = std::make_shared<spic::Sprite>("resources/sprites/tiles/" + texture, false, false, 100, 1);
