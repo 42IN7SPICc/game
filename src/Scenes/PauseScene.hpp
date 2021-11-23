a87cbf736bb61940f36aa394e3caef6f05852bab
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-23 16:36:43 +0100

    Create PauseScene

---
+++ src/Scenes/PauseScene.hpp
@@ -0,0 +1,15 @@
+#ifndef SPIC_GAME_PAUSESCENE_HPP
+#define SPIC_GAME_PAUSESCENE_HPP
+
+#include "Scene.hpp"
+
+namespace game
+{
+    class PauseScene : spic::Scene
+    {
+        public:
+            PauseScene();
+    };
+}
+
+#endif //SPIC_GAME_PAUSESCENE_HPP
