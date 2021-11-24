705b8c203a05d9c376e92e7c4672561473cf2dc9
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-24 14:11:04 +0100

    Create EnemyName.hpp

---
+++ src/Enums/EnemyName.hpp
@@ -0,0 +1,18 @@
+#ifndef SPIC_GAME_ENEMYNAME_HPP
+#define SPIC_GAME_ENEMYNAME_HPP
+
+namespace game
+{
+    enum class EnemyName
+    {
+        Panzer,
+        Gruppenfuhrer,
+        Schutze,
+        Erkunder,
+        GhillieAnzugSchutze,
+        Stabsarzt,
+        Raupenschlepper,
+    };
+}
+
+#endif //SPIC_GAME_ENEMYNAME_HPP
