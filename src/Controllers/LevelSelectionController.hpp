7496166ee6053a0fdecaaa0b6d90d87749d21c63
Tommy Hosewol <t.hosewol@gmail.com>
2021-11-24 15:48:35 +0100

    LevelSelectionController instead of LevelController

--- src/Controllers/LevelSelectionController.hpp
+++ src/Controllers/LevelSelectionController.hpp
@@ -1,28 +1,29 @@
-#ifndef SPIC_GAME_LEVELCONTROLLER_HPP
-#define SPIC_GAME_LEVELCONTROLLER_HPP
+#ifndef SPIC_GAME_LEVELSELECTIONCONTROLLER_HPP
+#define SPIC_GAME_LEVELSELECTIONCONTROLLER_HPP
 
 #include <map>
 #include <string>
 
 #include "BehaviourScript.hpp"
-//#include "rapidjson/document.h"
 #include "../Structs/Level.hpp"
+#include "../Structs/LevelWithTiles.hpp"
+#include "../Structs/Tile.hpp"
 
 namespace game
 {
-    class LevelController : spic::BehaviourScript
+    class LevelSelectionController : spic::BehaviourScript
     {
         public:
             void InitializeLevels();
 
-            std::shared_ptr<spic::GameObject> GetLevelGameObject(const std::string& fileName);
+            LevelWithTiles LoadLevel(const std::string& file);
 
             Level GetLevelDto(const std::string& levelName);
 
         private:
             void InitializeLevel(const std::string& file, const std::string& name);
 
-//            rapidjson::Document LoadFile(const std::string& fileName);
+            static std::string LoadFile(const std::string& fileName);
 
             std::map<std::string, Level> _levels{};
     };
