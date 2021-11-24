15909fd25d2cfb1fd4b5266172adc0334a7f4996
daanh432 <daan@daanhendriks.nl>
2021-11-24 16:33:06 +0100

    Implemented the victory screen

---
+++ src/Scenes/VictoryScene.hpp
@@ -0,0 +1,17 @@
+#ifndef SPIC_GAME_VICTORYSCENE_HPP
+#define SPIC_GAME_VICTORYSCENE_HPP
+
+#include "MenuScene.hpp"
+
+namespace game
+{
+    class VictoryScene : public MenuScene
+    {
+        private:
+            std::shared_ptr<spic::GameObject> GenerateConfetti();
+        public:
+            VictoryScene();
+    };
+}
+
+#endif //SPIC_GAME_VICTORYSCENE_HPP
