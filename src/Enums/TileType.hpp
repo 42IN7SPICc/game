8f4d62d184519d964849ccf02f27a09e522cb48d
daanh432 <daan@daanhendriks.nl>
2021-11-24 16:54:11 +0100

    Moved tile type

--- src/Enums/TileType.hpp
+++ src/Enums/TileType.hpp
@@ -1,17 +1,19 @@
 #ifndef SPIC_GAME_TILETYPE_HPP
 #define SPIC_GAME_TILETYPE_HPP
 
-namespace game {
-    enum TileType {
-        Bridge,
-        Bushes,
-        Grass,
-        Rocks,
-        Sand,
-        Street,
-        Water,
-        Start,
-        End
+namespace game
+{
+    enum TileType
+    {
+        Bushes = 0,
+        Grass = 1,
+        Street = 2,
+        Sand = 3,
+        Water = 4,
+        Bridge = 5,
+        Rocks = 6,
+        Start = 7,
+        End = 8,
     };
 }
 
