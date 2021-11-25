3811ee707b76b1e4b6213b72d882f5d2dc275d2e
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-25 13:02:42 +0100

    Update PointUtil

--- src/Utils/PointUtil.hpp
+++ src/Utils/PointUtil.hpp
@@ -9,6 +9,12 @@ namespace game
     {
         public:
             static spic::Point CalculateDirectionalPoint(const spic::Point& from, const spic::Point& to, double multiplier = 1.0);
+
+            static double Distance(const spic::Point& from, const spic::Point& to);
+
+            static double Distance(const spic::Point& pointDiff);
+
+            static spic::Point PointDiff(const spic::Point& point1, const spic::Point& point2);
     };
 }
 
