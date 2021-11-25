e1556831d93b0bb58e6b7a6dcbc2ff0d0f2d6663
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-25 15:51:05 +0100

    Create SortingLayer.hpp

---
+++ src/Enums/SortingLayer.hpp
@@ -0,0 +1,16 @@
+#ifndef SPIC_GAME_SORTINGLAYER_HPP
+#define SPIC_GAME_SORTINGLAYER_HPP
+
+namespace game
+{
+    enum SortingLayer
+    {
+        Map = 10,
+        Tower = 20,
+        Enemy = 40,
+        Hero = 60,
+        Bullet = 80,
+    };
+}
+
+#endif //SPIC_GAME_SORTINGLAYER_HPP
