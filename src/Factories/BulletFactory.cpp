82e3993c43d71564007463efc0a182398dad738a
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-26 15:40:53 +0100

    Update BulletFactory

--- src/Factories/BulletFactory.cpp
+++ src/Factories/BulletFactory.cpp
@@ -13,7 +13,7 @@
 
 using namespace game;
 
-std::shared_ptr<spic::GameObject> BulletFactory::CreateBullet(BulletType bulletType, const spic::Point& position, const std::string& targetTag, const spic::Point& direction, int range, int damage)
+std::shared_ptr<spic::GameObject> BulletFactory::CreateBullet(BulletType bulletType, const spic::Point& position, const std::string& targetTag, const spic::Point& direction, int range, int damage, double bombRadius)
 {
     auto bullet = std::make_shared<spic::GameObject>("bullet", "bullet", Layer::Game);
     bullet->Transform().position = position;
@@ -25,7 +25,7 @@ std::shared_ptr<spic::GameObject> BulletFactory::CreateBullet(BulletType bulletT
 
     // Behaviour scripts
     GameObjectUtil::LinkComponent(bullet, std::make_shared<BulletBehaviour>(bulletType, direction, range));
-    GameObjectUtil::LinkComponent(bullet, std::make_shared<DamageBehaviour>(damage, targetTag));
+    GameObjectUtil::LinkComponent(bullet, std::make_shared<DamageBehaviour>(damage, targetTag, (bulletType == BulletType::Bomb ? bombRadius : 0)));
 
     // Sprite
     GameObjectUtil::LinkComponent(bullet, std::make_shared<spic::Sprite>(GetBulletSprite(bulletType), false, false, SortingLayer::Bullet, 0));
