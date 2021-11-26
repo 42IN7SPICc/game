0330438361e6e6b94138622ee10059e41b3d2501
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-26 14:45:45 +0100

    Create BulletType.hpp

---
+++ src/Enums/BulletType.hpp
@@ -0,0 +1,14 @@
+#ifndef SPIC_GAME_BULLETTYPE_HPP
+#define SPIC_GAME_BULLETTYPE_HPP
+
+namespace game
+{
+    enum class BulletType
+    {
+        Normal,
+        Penetrating,
+        Bomb,
+    };
+}
+
+#endif //SPIC_GAME_BULLETTYPE_HPP
