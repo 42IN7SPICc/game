8bd9deb26892a57cb834525a4877c0109faedd72
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-29 15:54:11 +0100

    Update TowerPrefabFactory.cpp

--- src/Factories/TowerPrefabFactory.cpp
+++ src/Factories/TowerPrefabFactory.cpp
@@ -36,6 +36,8 @@ std::shared_ptr<spic::GameObject> TowerPrefabFactory::CreateBaseTower(const type
     auto shootingAnimator = std::make_shared<spic::Animator>(static_cast<int>(shootingSprites.size()), shootingSprites);
     GameObjectUtil::LinkComponent(baseTower, shootingAnimator);
 
+    attackBehaviour->FollowTarget(true);
+    attackBehaviour->Animator(shootingAnimator);
     GameObjectUtil::LinkComponent(baseTower, attackBehaviour);
 
     return baseTower;
@@ -56,7 +58,7 @@ std::shared_ptr<spic::GameObject> TowerPrefabFactory::CreateShotgun()
 {
     types::sprite_vector shootingSprites = AnimatorUtil::CreateSpriteVector(1, "resources/sprites/towers/shooting/tower_shooting_", SortingLayer::Enemy);
 
-    auto attackBehaviour = std::make_shared<AttackBehaviour>("enemy", BulletType::Normal, 5, 250, 20, 15, 0);
+    auto attackBehaviour = std::make_shared<AttackBehaviour>("enemy", BulletType::Normal, 5, 250, 20, 15);
 
     auto tower = CreateBaseTower(shootingSprites, attackBehaviour);
 
@@ -67,7 +69,7 @@ std::shared_ptr<spic::GameObject> TowerPrefabFactory::CreateFlamethrower()
 {
     types::sprite_vector shootingSprites = AnimatorUtil::CreateSpriteVector(1, "resources/sprites/towers/shooting/tower_shooting_", SortingLayer::Enemy);
 
-    auto attackBehaviour = std::make_shared<AttackBehaviour>("enemy", BulletType::Penetrating, 3, 150, 2, 20, 0);
+    auto attackBehaviour = std::make_shared<AttackBehaviour>("enemy", BulletType::Penetrating, 3, 150, 2, 20);
 
     auto tower = CreateBaseTower(shootingSprites, attackBehaviour);
 
@@ -78,7 +80,7 @@ std::shared_ptr<spic::GameObject> TowerPrefabFactory::CreateSniper()
 {
     types::sprite_vector shootingSprites = AnimatorUtil::CreateSpriteVector(1, "resources/sprites/towers/shooting/tower_shooting_", SortingLayer::Enemy);
 
-    auto attackBehaviour = std::make_shared<AttackBehaviour>("enemy", BulletType::Normal, 8, 500, 50, 30, 0);
+    auto attackBehaviour = std::make_shared<AttackBehaviour>("hero", BulletType::Normal, 8, 500, 50, 30);
 
     auto tower = CreateBaseTower(shootingSprites, attackBehaviour);
 
