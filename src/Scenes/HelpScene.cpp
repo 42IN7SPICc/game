466b97ca0d4c1810d5f3e2cd1b8796371ce824f3
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-29 12:07:40 +0100

    Update HelpScene.cpp

--- src/Scenes/HelpScene.cpp
+++ src/Scenes/HelpScene.cpp
@@ -14,10 +14,24 @@ using namespace spic;
 HelpScene::HelpScene() : MenuScene("Help", true)
 {
     auto creditsText = std::make_shared<Text>("Help Text", "text_help", Layer::HUD, 533, 400);
-    creditsText->Transform().position = {366.5, 450};
+    creditsText->Transform().position = {366.5, 400};
     creditsText->Size(24);
     creditsText->TextColor(Color::white());
-    creditsText->Content("Het spel wordt een Tower Defence game. Dit valt onder de categorie strategy games. In het geval van Avans Wars WW2 zal het een wave based strategy game zijn. Het doel van Tower Defence games is het voorkomen dat vijanden tot de eindlocatie kunnen komen door torens te plaatsen die schade aanbrengen aan de vijanden en deze te verslaan.");
+    creditsText->Content("Het spel wordt een Tower Defence game. Dit valt onder de categorie strategy games. In het geval van Avans Wars WW2 zal het een wave based strategy game zijn. Het doel van Tower Defence games is het voorkomen dat vijanden tot de eindlocatie kunnen komen door torens te plaatsen die schade aanbrengen aan de vijanden en deze te verslaan.\n\n\n\n");
+
+    auto controlsText = std::make_shared<Text>("Controls Text", "text_controls", Layer::HUD, 266.5, 400);
+    controlsText->Transform().position = {866.25, 400};
+    controlsText->Size(24);
+    controlsText->TextColor(Color::white());
+    controlsText->Content("Action\n\nPause/Resume\n\nHero Movement\nHero Ability\nHero Shooting\nHero Shoot Direction\n\nTile Selection\nTile Interaction");
+
+    auto controlsKeys = std::make_shared<Text>("Controls Text", "text_controls", Layer::HUD, 266.5, 400);
+    controlsKeys->Transform().position = {1132.75, 400};
+    controlsKeys->Size(24);
+    controlsKeys->TextColor(Color::white());
+    controlsKeys->Content("Control\n\nESC\n\nWASD\nE\nSPACE\nMouse Position\n\nMouse Position\nLeft Mouse Button");
 
     Contents().push_back(creditsText);
+    Contents().push_back(controlsText);
+    Contents().push_back(controlsKeys);
 }
