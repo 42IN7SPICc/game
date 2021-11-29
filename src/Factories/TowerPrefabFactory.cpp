dfa81de5323411389489d8f9f00b642d59679edb
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-29 13:13:36 +0100

    Update TowerPrefabFactory.cpp

--- src/Factories/TowerPrefabFactory.cpp
+++ src/Factories/TowerPrefabFactory.cpp
@@ -1,9 +1,24 @@
 #include "TowerPrefabFactory.hpp"
 
+#include <stdexcept>
+
 using namespace game;
 
 std::shared_ptr<spic::GameObject> TowerPrefabFactory::CreateTower(TowerName name)
 {
+    switch (name)
+    {
+        case TowerName::Bomber:
+            return CreateBomber();
+        case TowerName::Shotgun:
+            return CreateShotgun();
+        case TowerName::Flamethrower:
+            return CreateFlamethrower();
+        case TowerName::Sniper:
+            return CreateSniper();
+    }
+
+    throw std::runtime_error("Tower not implemented.");
 }
 
 std::shared_ptr<spic::GameObject> TowerPrefabFactory::CreateBaseTower()
@@ -12,16 +27,28 @@ std::shared_ptr<spic::GameObject> TowerPrefabFactory::CreateBaseTower()
 
 std::shared_ptr<spic::GameObject> TowerPrefabFactory::CreateBomber()
 {
+    auto tower = CreateBaseTower();
+
+    return tower;
 }
 
 std::shared_ptr<spic::GameObject> TowerPrefabFactory::CreateShotgun()
 {
+    auto tower = CreateBaseTower();
+
+    return tower;
 }
 
 std::shared_ptr<spic::GameObject> TowerPrefabFactory::CreateFlamethrower()
 {
+    auto tower = CreateBaseTower();
+
+    return tower;
 }
 
 std::shared_ptr<spic::GameObject> TowerPrefabFactory::CreateSniper()
 {
+    auto tower = CreateBaseTower();
+
+    return tower;
 }
