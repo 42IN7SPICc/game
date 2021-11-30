30f56435cbc5479244741036a669203778bd0024
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-30 20:29:45 +0100

    Add comments to utils

--- src/Utils/TileUtil.hpp
+++ src/Utils/TileUtil.hpp
@@ -6,11 +6,24 @@
 
 namespace game
 {
+    /**
+     * @brief A util to handle tiles.
+     */
     class TileUtil
     {
         public:
+            /**
+             * @brief Get the sprite corresponding to a given tile type.
+             * @param tileType The type of tile.
+             * @return A path to the sprite.
+             */
             static std::string GetSprite(const game::TileType& tileType);
 
+            /**
+             * @brief Get the tile type corresponding to a given sprite.
+             * @param sprite The path to the sprite.
+             * @return The type of the tile.
+             */
             static game::TileType GetTileType(const std::string& sprite);
     };
 }
