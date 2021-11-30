01f00d884a4c9da1a6d0bc89cb115d301f011cd8
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-11-30 13:38:31 +0100

    Moved Constants from LevelController to Global Contants

---
+++ src/Constants.hpp
@@ -0,0 +1,13 @@
+#ifndef SPIC_GAME_CONSTANTS_HPP
+#define SPIC_GAME_CONSTANTS_HPP
+
+const double TileSize = 32;
+const double TileButtonScale = 2.0;
+const double TileMapScale = 0.985;
+const int MapX = TileSize / 2 + 155;
+const int MapY = TileSize / 2;
+const int ScreenWidth = 1366;
+const int ScreenHeight = 786;
+const int HudWidth = 250;
+
+#endif
