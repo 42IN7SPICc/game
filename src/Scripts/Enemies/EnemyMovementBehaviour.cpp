d0fab7c18bef8fde9a850641868ebd4680a1e598
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-30 11:20:16 +0100

    Update EnemyMovementBehaviour.cpp

--- src/Scripts/Enemies/EnemyMovementBehaviour.cpp
+++ src/Scripts/Enemies/EnemyMovementBehaviour.cpp
@@ -98,6 +98,7 @@ void EnemyMovementBehaviour::OnUpdate()
 
     _walkingAnimator->Play(true);
     _sprite->FlipX(force.x < 0);
+    parent->Transform().rotation = 0;
 }
 
 void EnemyMovementBehaviour::OnTriggerEnter2D(const spic::Collider& collider)
