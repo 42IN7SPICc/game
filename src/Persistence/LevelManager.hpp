03f985d382148efb7ced38703cd1b2be2438f012
Sander Jochems <sanderjochems@hotmail.nl>
2021-11-30 19:05:11 +0100

    Add comments for managers

--- src/Persistence/LevelManager.hpp
+++ src/Persistence/LevelManager.hpp
@@ -8,13 +8,30 @@
 
 namespace game
 {
+    /**
+     * @brief Manager for loading Levels with or without tiles.
+     */
     class LevelManager
     {
         public:
+            /**
+             * @brief Load a level including the tiles.
+             * @param file The file to load.
+             * @return A level with tiles.
+             */
             static LevelWithTiles LoadLevelWithTiles(const std::string& file);
 
+            /**
+             * @brief Load a level without tiles.
+             * @param file The file to load.
+             * @return A level without tiles.
+             */
             static Level LoadLevel(const std::string& file);
 
+            /**
+             * @brief Load all levels without tiles.
+             * @return A list of filenames and the matching level.
+             */
             static std::map<std::string, Level> GetAll();
     };
 }
--- src/Persistence/SaveGameManager.hpp
+++ src/Persistence/SaveGameManager.hpp
@@ -7,13 +7,29 @@
 
 namespace game
 {
+    /**
+     * @brief A manager to save and load saves.
+     */
     class SaveGameManager
     {
         public:
+            /**
+             * @brief Save the player data to a file.
+             * @param playerData The player data to be saved.
+             */
             static void Save(const PlayerData& playerData);
 
+            /**
+             * @brief Load player data from a file.
+             * @param file The file to load the data from.
+             * @return The player data from the file.
+             */
             static PlayerData Load(const std::string& file);
 
+            /**
+             * @brief Load all saves with player data.
+             * @return A list of filenames and their player data.
+             */
             static std::map<std::string, PlayerData> GetAll();
     };
 }
