c5c9a1b818e5cc92f6b6892038546aadff21902f
Sander Jochems <sanderjochems@hotmail.nl>
2021-12-07 13:18:23 +0100

    Update EnemySpeedBoostBehaviour.hpp

--- src/Scripts/Enemies/EnemySpeedBoostBehaviour.hpp
+++ src/Scripts/Enemies/EnemySpeedBoostBehaviour.hpp
@@ -12,7 +12,7 @@ namespace game
     /**
      * @brief A behaviour script to boost other enemies in a certain range.
      */
-    class EnemySpeedBoostBehaviour : spic::BehaviourScript
+    class EnemySpeedBoostBehaviour : public spic::BehaviourScript
     {
         private:
             std::shared_ptr<CoolDownBehaviour> _coolDownBehaviour;
@@ -22,6 +22,13 @@ namespace game
             double _boostRange;
 
         public:
+            /**
+             * @brief Constructs a new instance of a EnemySpeedBoostBehaviour with given settings.
+             * @param boostCoolDown The time it takes to boost again (in seconds).
+             * @param speedBoost The multiplier for the speed of the enemy.
+             * @param boostTime The time the boost effect lasts (in seconds).
+             * @param boostRange The range enemies have to be in to get the effect.
+             */
             EnemySpeedBoostBehaviour(int boostCoolDown, double speedBoost, int boostTime, double boostRange);
 
             /**
