f104744cf9ea4829508ed6ddef8a24557ec9ad3a
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-12-07 16:59:15 +0100

    Implemented Increase Tower Range Ability for Franklin D Roosevelt

--- src/Scripts/Heroes/IncreaseTowerRangeAbilityBehaviour.cpp
+++ src/Scripts/Heroes/IncreaseTowerRangeAbilityBehaviour.cpp
@@ -1,4 +1,17 @@
 #include "IncreaseTowerRangeAbilityBehaviour.hpp"
+#include "../../Constants.hpp"
+#include "GameObject.hpp"
+#include "Input.hpp"
+#include "Engine.hpp"
+#include "../../Enums/Layer.hpp"
+#include "../Common/AttackBehaviour.hpp"
+#include "../../Utils/GameObjectUtil.hpp"
+
+game::IncreaseTowerRangeAbilityBehaviour::IncreaseTowerRangeAbilityBehaviour() : _coolDownBehaviour(std::make_shared<CoolDownBehaviour>(CoolDownBehaviour(FranklinDRooseveltTowerRangeAbilityCooldown))),
+                                                                                 _abilityActivated(false)
+{
+
+}
 
 void game::IncreaseTowerRangeAbilityBehaviour::OnStart()
 {
@@ -7,7 +20,36 @@ void game::IncreaseTowerRangeAbilityBehaviour::OnStart()
 
 void game::IncreaseTowerRangeAbilityBehaviour::OnUpdate()
 {
-    //
+    if(_abilityActivated) {
+        auto towerRangeObject = spic::GameObject::Find("towerRangeObject");
+        if (towerRangeObject && towerRangeObject->GetComponent<CoolDownBehaviour>()->CooledDown())
+        {
+            towerRangeObject->Destroy(towerRangeObject);
+            _abilityActivated = false;
+            auto towers = spic::GameObject::FindGameObjectsWithTag("tower");
+            for(auto& tower : towers) {
+                auto attackBehaviour = tower->GetComponent<AttackBehaviour>();
+                attackBehaviour->Range(attackBehaviour->Range() * 0.834);
+            }
+        }
+        return;
+    }
+
+    if (spic::Input::GetKeyDown(spic::Input::KeyCode::Q))
+    {
+        _abilityActivated = true;
+        auto scene = spic::Engine::Instance().PeekScene();
+        auto towerRangeObject = std::make_shared<spic::GameObject>("towerRangeObject", "towerRangeObject", Layer::Game);
+        auto towerRangeCooldown = std::make_shared<CoolDownBehaviour>(10);
+        GameObjectUtil::LinkComponent(towerRangeObject, towerRangeCooldown);
+
+        auto towers = spic::GameObject::FindGameObjectsWithTag("tower");
+        for(auto& tower : towers) {
+            auto attackBehaviour = tower->GetComponent<AttackBehaviour>();
+            attackBehaviour->Range(attackBehaviour->Range() * 1.2);
+        }
+        scene->Contents().push_back(towerRangeObject);
+    }
 }
 
 void game::IncreaseTowerRangeAbilityBehaviour::OnTriggerEnter2D(const spic::Collider& collider)
