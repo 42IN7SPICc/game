222be2eaf114543871e0423a19f14083696f732c
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-12-09 10:20:21 +0100

    Implemented Invisibility Ability

--- src/Scripts/Heroes/InvisibilityAbilityBehaviour.cpp
+++ src/Scripts/Heroes/InvisibilityAbilityBehaviour.cpp
@@ -1,13 +1,61 @@
 #include "InvisibilityAbilityBehaviour.hpp"
+#include "../../Constants.hpp"
+#include <stdexcept>
+#include "GameObject.hpp"
+#include "Engine.hpp"
+#include "Input.hpp"
+#include "../../Utils/GameObjectUtil.hpp"
+#include "../../Enums/Layer.hpp"
+
+game::InvisibilityAbilityBehaviour::InvisibilityAbilityBehaviour() : _coolDownBehaviour(std::make_shared<CoolDownBehaviour>(CoolDownBehaviour(BernardIJzerdraatAbilityCooldown))),
+                                                                     _abilityActive(false)
+{
+
+}
 
 void game::InvisibilityAbilityBehaviour::OnStart()
 {
-    //
+    auto parent = GameObject().lock();
+    _healthBehaviour = parent->GetComponent<game::HealthBehaviour>();
+    if (!_healthBehaviour)
+    {
+        throw std::runtime_error("The EnemyInvisibleBehaviour requires a HealthBehaviour");
+    }
+    _sprite = parent->GetComponent<spic::Sprite>();
+    game::GameObjectUtil::LinkComponent(parent, _coolDownBehaviour);
 }
 
 void game::InvisibilityAbilityBehaviour::OnUpdate()
 {
-    //
+    if (_abilityActive)
+    {
+        _sprite->Color(spic::Color(1, 1, 1, 0.5));
+        auto invisibilityObject = spic::GameObject::Find("invisibilityDurationObject");
+        if (invisibilityObject && invisibilityObject->GetComponent<CoolDownBehaviour>()->CooledDown())
+        {
+            invisibilityObject->Destroy(invisibilityObject);
+            _abilityActive = false;
+            _coolDownBehaviour->CooledDown(false);
+            _healthBehaviour->Invincibility(false);
+            _sprite->Color(spic::Color::transparent());
+        }
+        return;
+    }
+
+    if (spic::Input::GetKey(spic::Input::KeyCode::E))
+    {
+        if (_coolDownBehaviour->CooledDown())
+        {
+            _abilityActive = true;
+            _healthBehaviour->Invincibility(true);
+            auto invisibilityDurationObject = std::make_shared<spic::GameObject>("invisibilityDurationObject", "invisibilityDurationObject", Layer::Game);
+            auto invisibilityDurationCooldown = std::make_shared<CoolDownBehaviour>(BernardIJzerdraatAbilityDuration);
+            game::GameObjectUtil::LinkComponent(invisibilityDurationObject, invisibilityDurationCooldown);
+
+            auto scene = spic::Engine::Instance().PeekScene();
+            scene->Contents().push_back(invisibilityDurationObject);
+        }
+    }
 }
 
 void game::InvisibilityAbilityBehaviour::OnTriggerEnter2D(const spic::Collider& collider)
--- src/Scripts/Heroes/InvisibilityAbilityBehaviour.hpp
+++ src/Scripts/Heroes/InvisibilityAbilityBehaviour.hpp
@@ -2,11 +2,21 @@
 #define SPIC_GAME_INVISIBILITYABILITYBEHAVIOUR_HPP
 
 #include "BehaviourScript.hpp"
+#include "../Common/CoolDownBehaviour.hpp"
+#include "../Common/HealthBehaviour.hpp"
 
 namespace game {
     class InvisibilityAbilityBehaviour : public spic::BehaviourScript
     {
+        private:
+            std::shared_ptr<game::CoolDownBehaviour> _coolDownBehaviour;
+            std::shared_ptr<HealthBehaviour> _healthBehaviour;
+            std::shared_ptr<spic::Sprite> _sprite;
+            bool _abilityActive;
+
         public:
+            InvisibilityAbilityBehaviour();
+
             void OnStart() override;
 
             void OnUpdate() override;
