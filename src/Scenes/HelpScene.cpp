bd0960d5a5b89022b106dcbebec35426402f51b0
Sander Jochems <sanderjochems@hotmail.nl>
2021-12-10 15:47:53 +0100

    Update HelpScene

--- src/Scenes/HelpScene.cpp
+++ src/Scenes/HelpScene.cpp
@@ -1,6 +1,5 @@
 #include "HelpScene.hpp"
 
-#include "../Factories/ButtonPrefabFactory.hpp"
 #include "../Factories/BackgroundPrefabFactory.hpp"
 #include "../Enums/Layer.hpp"
 
@@ -17,7 +16,7 @@ HelpScene::HelpScene(const std::shared_ptr<spic::GameObject>& audio) : MenuScene
     creditsText->Transform().position = {366.5, 400};
     creditsText->Size(24);
     creditsText->TextColor(Color::white());
-    creditsText->Content("Het spel wordt een Tower Defence game. Dit valt onder de categorie strategy games. In het geval van Avans Wars WW2 zal het een wave based strategy game zijn. Het doel van Tower Defence games is het voorkomen dat vijanden tot de eindlocatie kunnen komen door torens te plaatsen die schade aanbrengen aan de vijanden en deze te verslaan.\n\n\n\n");
+    creditsText->Content("Avans Wars WW2 heeft als doel het verdedigen van het eindpunt. Leg een pad van het beginpunt naar het eindpunt, plaats verschillende torens in je land en verras de vijanden met je geweldige verdediging! Start de waves en gebruik de speciale vaardigheid van je held om de vijanden uit je land te jagen! Als er geen vijandelijke troepen meer zijn heb je gewonnen, maar als er te veel vijanden bij je eindpunt zijn binnengedrongen veroveren ze je land en heb je verloren... Ben jij in staat om je land te verslaan?\n");
 
     auto controlsText = std::make_shared<Text>("Controls Text", "text_controls", Layer::HUD, 266.5, 400);
     controlsText->Transform().position = {866.25, 400};
@@ -29,7 +28,7 @@ HelpScene::HelpScene(const std::shared_ptr<spic::GameObject>& audio) : MenuScene
     controlsKeys->Transform().position = {1132.75, 400};
     controlsKeys->Size(24);
     controlsKeys->TextColor(Color::white());
-    controlsKeys->Content("Toets\n\nESC\n\nWASD\nE\nSpatiebalk\nMuis positie\n\nMuis positie\nLinkermuisknop");
+    controlsKeys->Content("Toets\n\nESC\n\nWASD\nE\nLinkermuisknop\nMuis positie\n\nMuis positie\nLinkermuisknop");
 
     Contents().push_back(creditsText);
     Contents().push_back(controlsText);
