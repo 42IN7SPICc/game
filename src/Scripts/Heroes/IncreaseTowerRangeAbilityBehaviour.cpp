ea873772a7ffecf5dc3cf88a80d9ff2c02c20169
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-12-17 10:08:29 +0100

    Refactored TowerRangeAbilityBehaviour

--- src/Scripts/Heroes/IncreaseTowerRangeAbilityBehaviour.cpp
+++ src/Scripts/Heroes/IncreaseTowerRangeAbilityBehaviour.cpp
@@ -1,38 +1,33 @@
 #include "IncreaseTowerRangeAbilityBehaviour.hpp"
-#include "../../HeroConstants.hpp"
-#include "../../Factories/AudioSourcePrefabFactory.hpp"
 #include "GameObject.hpp"
 #include "Input.hpp"
 #include "Engine.hpp"
-#include "../../Enums/Layer.hpp"
-#include "../Common/AttackBehaviour.hpp"
 #include "Utils/GameObjectUtil.hpp"
+#include "../Common/AttackBehaviour.hpp"
+#include "../../Enums/Layer.hpp"
+#include "../../HeroConstants.hpp"
+#include "../../Factories/AudioSourcePrefabFactory.hpp"
 
-game::IncreaseTowerRangeAbilityBehaviour::IncreaseTowerRangeAbilityBehaviour() : _coolDownBehaviour(std::make_shared<CoolDownBehaviour>(CoolDownBehaviour(FranklinDRooseveltTowerRangeAbilityCooldown))),
+game::IncreaseTowerRangeAbilityBehaviour::IncreaseTowerRangeAbilityBehaviour() : AbilityBehaviour(FranklinDRooseveltTowerRangeAbilityCooldown),
                                                                                  _abilityActivated(false)
 {
 
 }
 
-void game::IncreaseTowerRangeAbilityBehaviour::OnStart()
-{
-    auto parent = GameObject().lock();
-    _healthBehaviour = parent->GetComponent<game::HealthBehaviour>();
-    spic::GameObjectUtil::LinkComponent(parent, _coolDownBehaviour);
-}
-
 void game::IncreaseTowerRangeAbilityBehaviour::OnUpdate()
 {
-    if(_healthBehaviour->Health() <= 0) return;
+    if (_healthBehaviour->Health() <= 0) return;
 
-    if(_abilityActivated) {
+    if (_abilityActivated)
+    {
         auto towerRangeObject = spic::GameObject::Find("towerRangeObject");
         if (towerRangeObject && towerRangeObject->GetComponent<CoolDownBehaviour>()->CooledDown())
         {
             towerRangeObject->Destroy(towerRangeObject);
             _abilityActivated = false;
             auto towers = spic::GameObject::FindGameObjectsWithTag("tower");
-            for(auto& tower : towers) {
+            for (auto& tower: towers)
+            {
                 auto attackBehaviour = tower->GetComponent<AttackBehaviour>();
                 attackBehaviour->Range(attackBehaviour->Range() * 0.834);
             }
@@ -63,24 +58,4 @@ void game::IncreaseTowerRangeAbilityBehaviour::OnUpdate()
             _coolDownBehaviour->CooledDown(false);
         }
     }
-}
-
-void game::IncreaseTowerRangeAbilityBehaviour::OnTriggerEnter2D(const spic::Collider& collider)
-{
-    //
-}
-
-void game::IncreaseTowerRangeAbilityBehaviour::OnTriggerExit2D(const spic::Collider& collider)
-{
-    //
-}
-
-void game::IncreaseTowerRangeAbilityBehaviour::OnTriggerStay2D(const spic::Collider& collider)
-{
-    //
-}
-
-std::shared_ptr<game::CoolDownBehaviour>& game::IncreaseTowerRangeAbilityBehaviour::GetCoolDownBehaviour()
-{
-    return _coolDownBehaviour;
 }
--- src/Scripts/Heroes/IncreaseTowerRangeAbilityBehaviour.hpp
+++ src/Scripts/Heroes/IncreaseTowerRangeAbilityBehaviour.hpp
@@ -1,20 +1,16 @@
 #ifndef SPIC_GAME_INCREASETOWERRANGEABILITY_HPP
 #define SPIC_GAME_INCREASETOWERRANGEABILITY_HPP
 
-#include "BehaviourScript.hpp"
-#include "../Common/CoolDownBehaviour.hpp"
-#include "../Common/HealthBehaviour.hpp"
+#include "AbilityBehaviour.hpp"
 
 namespace game
 {
     /**
      * @brief A script for handling the Tower Range ability for Franklin D Roosevelt
      */
-    class IncreaseTowerRangeAbilityBehaviour : public spic::BehaviourScript
+    class IncreaseTowerRangeAbilityBehaviour : public game::AbilityBehaviour
     {
         private:
-            std::shared_ptr<game::CoolDownBehaviour> _coolDownBehaviour;
-            std::shared_ptr<game::HealthBehaviour> _healthBehaviour;
             bool _abilityActivated;
 
         public:
@@ -24,38 +20,9 @@ namespace game
             IncreaseTowerRangeAbilityBehaviour();
 
             /**
-             * @brief Triggers when the scripts starts for the first time.
-             */
-            void OnStart() override;
-
-            /**
              * @brief Triggers every frame when the script is active.
              */
             void OnUpdate() override;
-
-            /**
-             * @brief Triggers when the script collides with a collider.
-             * @param collider The collider that collided.
-             */
-            void OnTriggerEnter2D(const spic::Collider& collider) override;
-
-            /**
-             * @brief Triggers when the script stopped colliding with a collider.
-             * @param collider The collider that stop colliding.
-             */
-            void OnTriggerExit2D(const spic::Collider& collider) override;
-
-            /**
-             * @brief Triggers while an collider is colliding with a collider.
-             * @param collider The collider is colliding.
-             */
-            void OnTriggerStay2D(const spic::Collider& collider) override;
-
-            /**
-             * Getter for the _coolDownBehaviour field
-             * @return the coolDownBehaviour
-             */
-            std::shared_ptr<game::CoolDownBehaviour>& GetCoolDownBehaviour();
     };
 }
 
