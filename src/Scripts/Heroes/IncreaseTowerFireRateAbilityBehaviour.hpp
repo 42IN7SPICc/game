43dc64b0421080d0c7f5296165d19231c3e96ef1
Max van de Vossenberg <maxvossenberg@gmail.com>
2021-12-17 10:08:16 +0100

    Refactored TowerFireRateAbilityBehaviour

--- src/Scripts/Heroes/IncreaseTowerFireRateAbilityBehaviour.hpp
+++ src/Scripts/Heroes/IncreaseTowerFireRateAbilityBehaviour.hpp
@@ -1,19 +1,15 @@
 #ifndef SPIC_GAME_INCREASETOWERFIRERATEABILITYBEHAVIOUR_HPP
 #define SPIC_GAME_INCREASETOWERFIRERATEABILITYBEHAVIOUR_HPP
 
-#include "BehaviourScript.hpp"
-#include "../Common/CoolDownBehaviour.hpp"
-#include "../Common/HealthBehaviour.hpp"
+#include "AbilityBehaviour.hpp"
 
 namespace game {
     /**
      * @brief A script for handling the Tower Fire Rate Ability for Winston Churchill
      */
-    class IncreaseTowerFireRateAbilityBehaviour : public spic::BehaviourScript
+    class IncreaseTowerFireRateAbilityBehaviour : public game::AbilityBehaviour
     {
         private:
-            std::shared_ptr<game::CoolDownBehaviour> _coolDownBehaviour;
-            std::shared_ptr<game::HealthBehaviour> _healthBehaviour;
             bool _abilityActivated;
 
         public:
@@ -23,38 +19,9 @@ namespace game {
             IncreaseTowerFireRateAbilityBehaviour();
 
             /**
-             * @brief Triggers when the scripts starts for the first time.
-             */
-            void OnStart() override;
-
-            /**
              * @brief Triggers every frame when the script is active.
              */
             void OnUpdate() override;
-
-            /**
-             * @brief Triggers when the script collides with a collider.
-             * @param collider The collider that collided.
-             */
-            void OnTriggerEnter2D(const spic::Collider& collider) override;
-
-            /**
-             * @brief Triggers when the script stopped colliding with a collider.
-             * @param collider The collider that stop colliding.
-             */
-            void OnTriggerExit2D(const spic::Collider& collider) override;
-
-            /**
-             * @brief Triggers while an collider is colliding with a collider.
-             * @param collider The collider is colliding.
-             */
-            void OnTriggerStay2D(const spic::Collider& collider) override;
-
-            /**
-             * Getter for the _coolDownBehaviour field
-             * @return the coolDownBehaviour
-             */
-            std::shared_ptr<game::CoolDownBehaviour>& GetCoolDownBehaviour();
     };
 }
 
